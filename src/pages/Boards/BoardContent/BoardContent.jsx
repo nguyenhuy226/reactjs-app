6637f7d5e681ee7ea772af5dbbd872bca8d29f09
nguyenhuy226 <nguyenhuyhuy04041998@gmail.com>
2024-04-28 17:05:09 +0700

    update

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,27 +1,105 @@
-import Box from "@mui/material/Box";
-import ListColumns from "./ListColumns/ListColumns";
-import { mapOrder } from "~/utils/sorts";
 import {
   DndContext,
+  MouseSensor,
+  TouchSensor,
   useSensor,
   useSensors,
-  PointerSensor,
+  DragOverlay,
+  defaultDropAnimationSideEffects,
 } from "@dnd-kit/core";
-import { useEffect, useState } from "react";
 import { arrayMove } from "@dnd-kit/sortable";
+import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { mapOrder } from "~/utils/sorts";
+import ListColumns from "./ListColumns/ListColumns";
+
+import Card from "./ListColumns/Column/ListCards/Card/Card";
+import Column from "./ListColumns/Column/Column";
+import styled from "@emotion/styled";
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: "ACTIVE_DRAG_ITEM_TYPE_COLUMN",
+  CARD: "ACTIVE_DRAG_ITEM_TYPE_CARD",
+};
 
 export default function BoardContent({ board }) {
+  // nếu dúng pinterSensor  mặc định thì phải kết hợp thuộc tích css touch-action: none ở những phần tử nào kéo thả - nhưng mà còn bug
+  // const pointerSensor = useSensor(PointerSensor, {
+  //   activationConstraint: { distance: 10 },
+  // });
+  // yêu cầu chuột di chuyển 10px thì mới kích hoạt event fix trường hợp click bị gọi event
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 },
+  });
+  // nhấn giữ 250ms và dung sai cảm ứng 500px thì mới kích hoạt evenr
+  const tounchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 250, tolerance: 500 },
+  });
+  // const mySensors = useSensors(pointerSensor);
+  // Ưu tiên sử dụng kết hợp 2 loại sensors là mouse và touch để có trải nghiệm  trên mobile tốt nhất, không bị bug
+  const mySensors = useSensors(mouseSensor, tounchSensor);
+
   const [orderedColumns, setOrderedColumns] = useState([]);
+  //cùng một thời điểm chỉ có một phần tử đang được kéo (column hoặc card)
+  const [activeDragItemId, setActiveDragItemId] = useState(null);
+  const [activeDragItemType, setActiveDragItemType] = useState(null);
+  const [activeDragItemData, setActiveDragItemData] = useState(null);
+
   useEffect(() => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
   }, [board]);
+  // tìm một column theo một idcard
+  const findColumnByCardId = (cardId) => {
+    // đoạn này cần lưu ý, nên dùng c.cards thay vì c.cardOderIds bời bì ỏ bước handleDragOver chúng ta sẽ làm dữ liệu cho cards hoàn chỉnh trước rồi mới tạo ra cardOrderIds mới
+    return orderedColumns.find((column) =>
+      column?.cards?.map((card) => card._id).includes(cardId)
+    );
+  };
+
+  // trigger khi bắt đầu kéo 1 phần tử
+  const handleDragStart = (event) => {
+    console.log(event);
+    setActiveDragItemId(event?.active?.id);
+    setActiveDragItemType(
+      event?.active?.data?.current?.columnId
+        ? ACTIVE_DRAG_ITEM_TYPE.CARD
+        : ACTIVE_DRAG_ITEM_TYPE.COLUMN
+    );
+    setActiveDragItemData(event?.active?.data?.current);
+  };
+  // trigger khi kéo 1 phần tử
+  const handleDragOver = (event) => {
+    // không làm gì thêm khi kéo column
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) {
+      return;
+    }
+    const { active, over } = event;
+    // kiểm tra nếu không tồn tại active hoặc over (kéo linh tinh râ ngoài  thì return  luôn tránh lỗi)
+    if (!over || !active) return;
+    // activecard là card đang kéo
+    const {
+      id: activeDraggingCardId,
+      data: { current: activeDraggingCardData },
+    } = active;
+    // overcarrd là card đang trong lúc tương tác trên hoặc dưới so với cái card  được kéo ở trên
+    const { id: overCardId } = over;
+
+    // tìm 2 cái column theo cardId
+    const activeColumn = findColumnByCardId(activeDraggingCardId);
+    const overColumn = findColumnByCardId(overCardId);
+
+    if (!activeColumn || !overColumn) return;
+  };
+  // trigger khi kêt thức hành động kéo 1 phần tử
   const handleDragEnd = (event) => {
-    console.log("handleDragEnd", event);
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      return;
+    }
     const { active, over } = event;
     // kiểm tra nếu không tồn tại over (kéo linh tinh râ ngoài  thì return  luôn tránh lỗi)
+    if (!over || !active) return;
 
     // nếu vị trí sau khi kéo thả khác với vị trí ban đầu
-    if (!over) return;
     if (active.id !== over.id) {
       // lấy vị trí cũ (từ thằng active)
       const oldIndex = orderedColumns.findIndex((c) => c._id === active.id);
@@ -36,11 +114,30 @@ export default function BoardContent({ board }) {
 
       // cập nhật lại state columns ban đầu sau khi đã kéo thả
       setOrderedColumns(dndOrderedColumns);
-      console.log(dndOrderedColumns);
     }
+    setActiveDragItemData(null);
+    setActiveDragItemId(null);
+    setActiveDragItemType(null);
   };
+
+  // Animation khi thả (Drog) phàn tử = test bằng cách kéo xong thả trực tiếp và nhìn phần giữ chỗ Overlay
+  const drogAnimation = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: {
+        active: {
+          opacity: "0.5",
+        },
+      },
+    }),
+  };
+
   return (
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
+      sensors={mySensors}
+    >
       <Box
         sx={{
           bgcolor: (theme) =>
@@ -51,6 +148,15 @@ export default function BoardContent({ board }) {
         }}
       >
         <ListColumns columns={orderedColumns} />
+        <DragOverlay dropAnimation={drogAnimation}>
+          {!activeDragItemType && null}
+          {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN && (
+            <Column column={activeDragItemData} />
+          )}
+          {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD && (
+            <Card card={activeDragItemData} />
+          )}
+        </DragOverlay>
       </Box>
     </DndContext>
   );
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -7,8 +7,29 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 function Card({ card }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: card._id, data: { ...card } });
+
+  const dndKitCardStyle = {
+    // touchAction: "none", // dành cho sensor default dạng pointerSensor
+
+    // nếu sử dụng CSS.Transform như docs sẽ lỗi kiểu stretch
+    // https://github.com/clauderic/dnd-kit/issues/117
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? "1px solid #2ecc71" : undefined,
+  };
+
   const shouldShowCardActions = () => {
     return (
       !!card?.memberIds?.length ||
@@ -18,6 +39,10 @@ function Card({ card }) {
   };
   return (
     <MuiCard
+      ref={setNodeRef}
+      style={dndKitCardStyle}
+      {...attributes}
+      {...listeners}
       sx={{
         cursor: "pointer",
         boxShadow: "0 1px 1px rgba(0, 0, 0, 0.2)",
