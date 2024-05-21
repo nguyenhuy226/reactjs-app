9593c3a5c52fdfa4a19493f1e5aab05121947aed
nguyenhuy226 <nguyenhuyhuy04041998@gmail.com>
2024-05-21 16:41:47 +0700

    upadte

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,13 @@
 // chứa các biến http
 // export const API_ROOT = "http://localhost:8017";
-export const API_ROOT = "https://node-api-2tsp.onrender.com";
+let apiRoot = "";
+console.log(import.meta.env);
+console.log(process.env);
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8017";
+}
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://node-api-2tsp.onrender.com";
+}
+console.log(apiRoot);
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
+  // cho phép thằng vite sử dụng được process.env
+  define: {
+    "process.env": process.env,
+  },
   plugins: [react(), svgr()],
   // base: './'
   resolve: {
