9defffa0c3fc7ea96db0d46983768bcf1726f569
nguyenhuy226 <nguyenhuyhuy04041998@gmail.com>
2024-05-21 16:44:25 +0700

    upadte

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  // cho phép thằng vite sử dụng được process.env
+  // cho phép thằng vite sử dụng được process.env, mặc định thì không mà sẽ phải dùng import.meta.env
   define: {
     "process.env": process.env,
   },
