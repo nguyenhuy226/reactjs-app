c275a4e2a61aae340b7467a94e6f309417f5baf4
nguyenhuy226 <nguyenhuyhuy04041998@gmail.com>
2024-05-21 15:49:37 +0700

    upadte

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 // chứa các biến http
-export const API_ROOT = "http://localhost:8017";
+// export const API_ROOT = "http://localhost:8017";
+export const API_ROOT = "https://node-api-2tsp.onrender.com";
